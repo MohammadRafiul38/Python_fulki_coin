63f0c66d7455721604700e28ddc7a65f9a5c1621
MohammadRafiul38 <mrafiulchowdhury@gmail.com>
2024-10-30 15:23:03 +0600

    Add files via upload

---
+++ fulki coin.py
@@ -0,0 +1,18 @@
+
+print("Welcome to the first Fulki Coin converter ")
+x = input("Input a Fulki Coin: ")
+fulki_coin = int(x) * 1200
+print("\033[92mThe value in Bangladeshi currency: ", fulki_coin)
+
+fulki_coin = int(x) * 10
+print("\033[91mValue in American currency: ", fulki_coin)
+
+
+fulki_coin_other = int(x) * 8
+print("\033[94mValue in British currecy: ", fulki_coin_other)
+
+fulki_coin_other = int(x) * 37
+print("\033[93mValue in UAE currency: ", fulki_coin_other)
+
+fulki_coin_other = int(x) * 846
+print("\033[96mValue in Indian currency: ", fulki_coin_other)
